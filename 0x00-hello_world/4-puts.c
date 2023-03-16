45ef05e0160de212c701040f9a78a554b8ffda7a
Abdotarek <abdo_bemo@hotmail.com>
2023-03-16 14:53:58 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-/*
-a program that displays a text
-*/
+/*a program that displays a text*/
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
