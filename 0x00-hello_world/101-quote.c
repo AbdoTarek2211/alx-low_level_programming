b19ca037f5e69551e0159349fc853519ddc8797c
Abdotarek <abdo_bemo@hotmail.com>
2023-03-16 15:44:01 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - A program that prints a line with puts function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
