7c9ff2b3049aad222346a0438c234f182eb0fbb6
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-03-17 21:58:04 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+#include <ctype.h>
+/**
+* main - A program that prints a line with puts function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int j = 0;
+for (; j <= 9; j++)
+{
+putchar('0' + j);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
