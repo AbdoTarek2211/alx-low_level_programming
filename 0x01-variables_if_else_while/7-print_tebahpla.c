cbc70352e4f50a99871cd9dcf3bc9cd87872dd45
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-03-17 21:51:36 +0200

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include <ctype.h>
+/**
+* main - A program that prints a line with puts function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int i = 'Z';
+for (; i >= 'A'; i--)
+{
+putchar(tolower(i));
+}
+putchar('\n');
+return (0);
+}
