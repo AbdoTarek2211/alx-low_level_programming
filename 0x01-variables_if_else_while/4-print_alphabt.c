c2163a121de81c2205e600002994700cd4d4e186
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-03-17 21:37:54 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+#include <ctype.h>
+/**
+* main - A program that prints a line with puts function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int i = 'A';
+for (; i <= 'Z'; i++)
+{
+if(i == 'E' || i == 'Q')
+{
+continue;
+}
+else
+{
+putchar(tolower(i));
+}
+}
+printf("\n");
+return (0);
+}
