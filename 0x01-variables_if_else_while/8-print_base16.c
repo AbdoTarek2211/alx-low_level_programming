121eed690181b4a93fbb62ee58016ef7c2402039
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-03-17 21:54:25 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+#include <ctype.h>
+/**
+* main - A program that prints a line with puts function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int i = 0;
+for (; i <= 9; i++)
+{
+printf("%d", i);
+}
+int i = 'A';
+for (; i <= 'F'; i++)
+{
+putchar(tolower(i));
+}
+putchar('\n');
+return (0);
+}
