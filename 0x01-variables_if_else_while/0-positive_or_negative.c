d9a39eeb9a9ec6e1079fdc62920b702305b01fd5
Abdotarek <abdo_bemo@hotmail.com>
2023-03-17 11:27:47 -0700

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <time.h>
+11;rgb:0000/0000/0000#include <time.h>
 #include <stdio.h>
 /**
 * main - A program that prints a line with puts function
@@ -11,10 +11,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
- printf("%d is positive\n", n);
+printf("%d is positive\n", n);
 else if (n < 0)
- printf("%d is negative\n", n);
+printf("%d is negative\n", n);
 else
- printf("%d is zero\n", n);
+printf("%d is zero\n", n);
 return (0);
 }
