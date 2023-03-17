6da09d0c2b56b2f081fedfbc5d78a518f72d5231
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-03-17 21:44:45 +0200

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+/**
+* main - A program that prints a line with puts function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int i = 0;
+for (; i <= 9; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
