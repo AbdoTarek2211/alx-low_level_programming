7555114690ef397e8c159787ebbb6e624617ea53
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-03-23 15:29:46 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
 * _isupper - checks for uppercase character
 * @c : the character to be checked
-* Returns 1 if @c is uppercase  0 otherwise
+* Returns 1 if @c is uppercase, 0 otherwise
 */
 int _isupper(int c)
 {
