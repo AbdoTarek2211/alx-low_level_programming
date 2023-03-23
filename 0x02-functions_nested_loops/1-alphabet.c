faeb9b71305c6bc22e1e7afa50839e29b3588220
Abdotarek <abdo_bemo@hotmail.com>
2023-03-22 17:11:33 -0700

    finally

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* Main Function returns 0 
+* print_alphabet - print all alphabet in lowercas
+*/
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
