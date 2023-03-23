1ab1bbc4bb4e572401795fe5998bba1994131f2d
Abdotarek <abdo_bemo@hotmail.com>
2023-03-23 06:16:28 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* checks for uppercase character.
+* Returns 1 if @c is uppercase
+* Returns 0 otherwise
+**/
+int main(void)
+{
+char c;
+if(_isupper(c))
+{
+return (1); 
+}
+else
+return (0);
+}
