bc219384f5e084edb396ddf969f8d96e8d1e4bb8
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-03-29 20:23:54 +0200

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * _strcpy - copies a string into another string
- * @src : string to be copied
- * @dest : string to be placed at
+ * leet - encodes a string into 1337.
+ * @s : string to be copied
  *
  * Return: 0 or 1
  */
