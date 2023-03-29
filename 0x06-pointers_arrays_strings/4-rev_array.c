0dda3d67457817575a348f14c54531e9c5909c2f
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-03-29 21:56:58 +0200

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,7 +11,12 @@ void reverse_array(int *a, int n)
 int temp = n - 1;
 int x;
 int y;
-for (x = 0; x <= n / 2; x++)
+int z;
+if (n % 2 == 0)
+z = n / 2;
+else
+z = (n / 2) + 1;
+for (x = 0; x <= z; x++)
 {
 y = a[x];
 a[x] = a[temp];
