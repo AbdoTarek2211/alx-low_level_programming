bb5c7ed958fa3e07ce800545ff668439b446a2ab
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-03-30 18:07:30 +0200

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,11 +9,11 @@ char *rot13(char *s)
 {
 int i;
 int j;
-char *x = "abcdefghijklmnopqrstuvwxyz";
-char *y = "nopqrstuvwxyzabcdefghijklm";
+char *x = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char *y = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; j < 26; j++)
+for (j = 0; j < 52; j++)
 {
 if (s[i] == x[j])
 s[i] = y[j];
