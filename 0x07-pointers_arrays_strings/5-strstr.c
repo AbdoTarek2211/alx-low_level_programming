8e5bcca7308d7e2b94eb682325de8b55b15e7daa
AbdoTarek <abdo_bemo@hotmail.com>
2023-04-03 08:25:51 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _strspn - Returns a pointer to the first occurrence of a character
+ * @s : string to find in
+ * @accept : character to be found
+ *
+ * Return: a string
+ */
+char *_strstr(char *haystack, char *needle)
