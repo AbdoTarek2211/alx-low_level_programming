7ceab571f8822b3c6c212ed5b05e8d3bb8e3360c
AbdoTarek <abdo_bemo@hotmail.com>
2023-04-03 08:02:28 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strspn - Returns a pointer to the first occurrence of a character
+ * @s : string to find in
+ * @accept : character to be found
+ *
+ * Return: a string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
+
+
+
+}
