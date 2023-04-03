9386764a40134b7d94a70458df59936b52240824
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-03 18:23:17 +0200

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,5 +6,30 @@
  */
 void print_diagsums(int *a, int size)
 {
-
+int x;
+int y;
+int temp = 0;
+int temp2 = 0;
+for (x = 0; x < size; x++)
+{
+for (y = 0; y < size; y++)
+{
+if (x == y)
+{
+temp += a[x][y];
+}
+}
+}
+_putchar(temp);
+int z = size - 1;
+for (x = 0; x < size; x++)
+{
+for (; z >= 0; z--)
+{
+temp2 += a[x][z];
+}
+}
+_putchar(',');
+_putchar(' ');
+_putchar(temp2);
 }
