07e0563a3edd4891e40664f7ced70e1473f76a94
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-05 20:48:21 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-  if (*s == '\0')
-    return;
-  _putchar(*s);
-  s++;
-  if (*s != '\0')
-    _puts_recursion (*s);
+if (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s++);
+}
+else
+_putchar('\n');
 }
