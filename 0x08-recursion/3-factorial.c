1e1b22175feb196e90c693c2d3bed89cbde2f349
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-05 21:45:50 +0200

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _strlen_recursion - returns the length of a string
- * @s : string to be printed
+ * factorial - returns the factorial of a given number.
+ * @n : number to be factorial
  *
  * Return: a string
  */
