8b6d0f3239c20821337843b0b3d7d3bb5f222615
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-05 22:33:40 +0200

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _pow_recursion - returns the factorial of a given number.
- * @n : number to be factorial
- *
+ * @x : number to be powered
+ * @y : number to be power of
  * Return: a string
  */
 int _pow_recursion(int x, int y)
