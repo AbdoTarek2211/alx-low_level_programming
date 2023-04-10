b042ff6b736b37e1ff35f496f8b17513885f813d
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-10 20:38:26 +0200

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: count of program
+ * @argv: number of arguments used
+ *
+ * Return: 1 if are less of 2 arguments otherwise 0.
+ */
+int main(int argc, char **argv)
+{
+int x;
+if (argc < 2 || argc > 2)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+x = atoi(*(argv + 1)) * atoi(*(argv + 2));
+printf("%i\n", x);
+}
+return (0);
+}
