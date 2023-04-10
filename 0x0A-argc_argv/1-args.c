b935bdad6058f5d0555e0d834211cc3baf68e984
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-10 19:45:10 +0200

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: count of program
+ * @argv: number of arguments used
+ *
+ * Return: always 0.
+ */
+int main(int argc, char **argv)
+{
+int x = 0;
+while (x < argc)
+{
+x++;
+argv++;
+}
+printf("%i\n", number - 1);
+return (0);
+}
