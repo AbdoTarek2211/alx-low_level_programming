bf6928362bafcff297260aa901e614bb722d4c4d
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-11 21:28:18 +0200

    Create 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * create_array - creates an array of chars, and initializes
+ * @size: size of array
+ * @c: array to be passsed
+ *
+ * Return: always 0.
+ */
+char *_strdup(char *str)
+{
+int strsize,tempsize;
+strsize = 0;
+char *temp;
+if (str == NULL)
+return (NULL);
+while (*str != '\0')
+{
+strsize++:
+str++;
+}
+temp = malloc(strsize * sizeof(char));
+for (int i = 0; i < strsize; i++)
+{
+*temp[i] = *str[i];
+}
+return (temp);
+}
