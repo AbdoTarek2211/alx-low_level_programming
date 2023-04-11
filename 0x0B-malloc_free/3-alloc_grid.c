fcca3e0ae5d24c066b84b81153ede2a5cc3e914b
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-11 22:38:15 +0200

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,15 +15,11 @@ int i, j;
 int **temp;
 if (width == 0 || height == 0 || width < 0 || height < 0)
 return (NULL);
-temp = malloc((width * height) *sizeof(int *));
+temp = (int **) malloc((width * height) *sizeof(int *));
 if (temp == NULL)
 return (NULL);
-for (i = 0; i < width; i++)
-{
-for (j = 0; j < height; j++)
-{
-temp[i * height + j] = 0;
-}
-}
+for (i = 0; i < height; i++)
+for (j = 0; j < width; j++)
+*(*(temp + i) + j) = 0;
 return (temp);
 }
