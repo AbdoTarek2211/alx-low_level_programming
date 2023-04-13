f0c7f98b1fb9c11038792559c18a1ed6757c0cad
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-13 22:44:40 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,20 +18,20 @@ s1 = "";
 if (s2 == NULL)
 s2 = "";
 for (x = 0; s1[x] != '\0'; x++)
+{
+}
 for (y = 0; s2[y] != '\0'; y++)
+{
+}
 if (n > y)
 n = y;
 newString = malloc(((x + n) + 1));
 if (temp == NULL)
 return (NULL);
 for (x = 0; s1[x] != '\0'; x++)
-{
 temp[x] = s1[x];
-}
 for (y = 0; y != n; y++, x++)
-{
 temp[a] = s2[y];
-}
 temp[x] = '\0';
 return (temp);
 }
