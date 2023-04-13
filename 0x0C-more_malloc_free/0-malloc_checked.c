f568ba94cd012830a260ab6f3b74b14b3e38a106
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-13 22:12:04 +0200

    malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of bytes
+ *
+ * Return: always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+char *temp;
+temp = malloc(b *sizeof(char));
+if (temp == NULL)
+printf(%d ,98);
+else
+return (temp);
+}
