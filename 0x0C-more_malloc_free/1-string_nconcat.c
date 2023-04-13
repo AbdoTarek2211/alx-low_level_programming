919dbc1a482d03bc0c226c5d69d7c81b8f4b29c8
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-13 22:40:55 +0200

    Concat strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,7 @@
 #include <stddef.h>
 #include <stdio.h>
 /**
- * string_nconcat - creates an array of chars, and initializes
+ * string_nconcat - concatenates two strings.
  * @s1: first string
  * @s2: second string
  * @n : size of characters
@@ -16,9 +16,9 @@ char *temp;
 int x = 0;
 int y = 0;
 if (s1 == NULL)
-*s1 = "";
+s1 = "";
 if (s2 == NULL)
-*s2 = "";
+s2 = "";
 while (*(s1 + x) != '\0')
 x++;
 while (*(s2 + y) != '\0')
