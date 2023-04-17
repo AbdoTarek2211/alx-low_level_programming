4499e3ff30ab39d58bb32185ec16c38fef2aff88
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-17 21:09:14 +0200

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_
+#define DOG_
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog__
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
