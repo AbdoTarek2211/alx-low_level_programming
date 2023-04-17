db947767b0b38eaff9145669a0fcb685703e2669
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-17 21:34:22 +0200

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,8 +8,8 @@
  */
 void print_dog(struct dog *d)
 {
-if (!d)
-return;
+if (d)
+{
 if (d->name == NULL)
 d->name = (nil);
 if (d->owner == NULL)
@@ -17,4 +17,5 @@ d->owner = (nil);
 printf("Name: %s\n", d->name);
 printf("Age: %d\n", d->age);
 printf("Owner: %s\n", d->owner);
+}
 }
