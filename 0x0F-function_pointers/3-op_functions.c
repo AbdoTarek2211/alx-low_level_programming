4d3caed19157ccbfd5f35bb38ea629735989cdab
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-20 22:29:51 +0200

    Create 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - add
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulus
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
