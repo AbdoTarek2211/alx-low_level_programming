d4ecb6194791b60ef56e981c947f87ea71f07144
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-20 21:40:20 +0200

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: array to be passed
+ * @size: size of array
+ * @cmp: function passed
+ *
+ * Return: 0 or 1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int x;
+if (size <= 0)
+return (-1);
+if (array == NULL)
+return (-1);
+if (cmp == NULL)
+return (-1);
+for (x = 0; x < size; i++)
+{
+if (cmp(array[x] >= 0)
+return (x);
+}
+return (-1);
+}
