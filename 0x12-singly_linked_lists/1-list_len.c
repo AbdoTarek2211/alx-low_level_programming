16ff6df5dd9ed01cae5a516fd2d94e5476bfb188
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-28 00:27:26 +0200

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - returns number of elements
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+size_t list_len(const list_t *h)
+{
+size_t counter = 0;
+if (h == NULL)
+return (0);
+while (h != NULL)
+{
+if (h->next == NULL)
+break;
+counter++;
+h = h->next;
+}
+return (counter);
+}
