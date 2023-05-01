e413dda0a1a8b9bb5cd5a2af5bfcb279199e75cf
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-05-01 20:24:14 +0300

    Create 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * pop_listint - returns number of elements
+ * @head: pointer to the head list to add to
+ *
+ * Return: the number of nodes printed
+ */
+int pop_listint(listint_t **head)
+{
+listint_t *temp;
+int value;
+if (head == NULL)
+return 0;
+value = (*head)->n;
+temp = (*head)->next;
+free(*head);
+(*head) = temp;
+return (value);
+}
