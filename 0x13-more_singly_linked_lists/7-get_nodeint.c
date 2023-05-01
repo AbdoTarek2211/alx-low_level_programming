a65c6b8f271334a2415ec0eedf88470899c63ec6
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-05-01 20:43:15 +0300

    Create 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * get_nodeint_at_index - returns the nth node
+ * @head: pointer to the head list
+ * @index: index of the node
+ * Return: the number of nodes printed
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+listint_t *temp = head;
+unsigned int x = 0;
+for (;x < index, temp; x++)
+{
+temp = temp->next;
+}
+if (temp == NULL)
+return (NULL);
+return (temp);
+}
