0060194339fb9acfd824f0862c71c4102604328e
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-05-01 21:04:54 +0300

    Create 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - returns number of elements
+ * @head: pointer to the head list to add to
+ * @idx: index of node
+ * @n: number to be added
+ * Return: the number of nodes printed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *temp;
+
+}
