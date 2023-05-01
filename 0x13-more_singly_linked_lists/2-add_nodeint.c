97b32028dec3bd1f9794f51bd53dc35afc5155af
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-05-01 19:12:53 +0300

    Create 2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_nodeint - returns number of elements
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *temp;
+unsigned int i = 0;
+temp = malloc(sizeof(listint_t));
+if (temp == NULL)
+return (NULL);
+temp->n = n;
+temp->next = (*head);
+(*head) = temp;
+return (temp);
+}
