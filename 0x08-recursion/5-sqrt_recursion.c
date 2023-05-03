2c2d7446e64298f0dafe19393ecf49a7ae6b9498
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-05-03 16:47:10 +0300

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the square root
+ * @n : number to be powered
+ * Return: 0 or 1
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+return (sq_rec(n, 0));
+}
+/**
+ * sq_rec - find natural number
+ * @n: number to calculate the sqaure root
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int sq_rec(int n, int i)
+{
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (sq_rec(n, i + 1));
+}
