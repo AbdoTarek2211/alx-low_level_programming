ce36d9d15968e95eb8680b3dc2bc2e1e763440c0
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-05-03 21:27:53 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,45 +3,13 @@
  * print_times_table - prints n table
  * @n: number of the table
  */
-void print_times_table(int n)
+void print_to_98(int n)
 {
-int x, y, z;
-if (n >= 0 && n <= 15)
-{
-for (x = 0; x <= n; x++)
-{
-for (y = 0; y <= n; y++)
-{
-z = y * x;
-if (y == 0)
-{
-_putchar(z + '0');
-}
-else if (z < 10 && y != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(z + '0');
-} else if (z >= 10 && z < 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((z / 10) + '0');
-_putchar((z % 10) + '0');
-}
-else if (z >= 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((z / 100) + '0');
-_putchar(((z / 10) % 10) + '0');
-_putchar((z % 10) + '0');
-}
-}
-_putchar('\n');
-}
-}
+if (n <= 98)
+for (; n <= 97; n++)
+printf("%d, ", n);
+else
+for (; n > 98; n--)
+printf("%d, ", n);
+printf("98\n");
 }
