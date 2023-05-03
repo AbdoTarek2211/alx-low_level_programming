a004716c5a87f73481e8c221cd10362cd0f8e526
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-05-03 16:54:34 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,20 +8,20 @@ int _sqrt_recursion(int n)
 {
 if (n < 0)
 return (-1);
-return (sq_recu(n, 0));
+return (actual_sqrt_recursion(n, 0));
 }
 /**
- * sq_rec - find natural number
- * @n: number to calculate the sqaure root
+ * actual_sqrt_recursion - find natural number
+ * @n: number to calculate the sqaure root of
  * @i: iterator
  *
- * Return: the resulting square root
+ * Return: -1 or 1
  */
-int sq_recu(int n, int i)
+int actual_sqrt_recursion(int n, int i)
 {
 if (i * i > n)
 return (-1);
 if (i * i == n)
 return (i);
-return (sq_rec(n, i + 1));
+return (actual_sqrt_recursion(n, i + 1));
 }
