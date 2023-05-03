e9fa5e596f7b2b8683400c78790ed632b02df16a
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-05-03 14:52:41 +0300

    Create 100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * reverse_listint - reverse a list
+ * @head: pointer to the listint_t head to print
+ *
+ * Return: the number of nodes printed
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *before = NULL;
+listint_t *after = NULL;
+while (*head)
+{
+after = (*head)->next;
+(*head)->next = before;
+before = (*head);
+(*head) = next;
+}
+(*head) = before;
+return (*head);
+}
