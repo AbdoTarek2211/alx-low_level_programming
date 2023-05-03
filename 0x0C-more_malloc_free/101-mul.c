65f7cbaa211a0e8b40dbf266033c966ababc6577
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-05-04 00:55:55 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -34,6 +34,14 @@ i++;
 return (i);
 }
 /**
+ * errorfunc - handles errors for main
+ */
+void errorfunc(void)
+{
+printf("Error\n");
+exit(98);
+}
+/**
  * main - multiplies two positive numbers
  * @argc: arg count
  * @argv: arg vector
@@ -47,8 +55,7 @@ int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 s1 = argv[1], s2 = argv[2];
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 {
-printf("Error\n");
-exit(98);
+errorfunc();
 }
 len1 = _strlen(s1);
 len2 = _strlen(s2);
@@ -65,7 +72,7 @@ carry = 0;
 for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
 digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 * digit2);
+carry += result[len1 + len2 + 1] + (digit1 *digit2);
 result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
 }
