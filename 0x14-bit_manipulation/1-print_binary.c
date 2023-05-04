a50006f7697cad67d7e0d2d90d5b599464d32696
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-05-05 00:48:00 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_binary - print binary representation of a number
- * @n: decimal number to print as binary
+ * print_binary - converts decimal to binary
+ * @n: number to be converted
  */
 void print_binary(unsigned long int n)
 {
